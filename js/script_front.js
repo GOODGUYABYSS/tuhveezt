f606c77e59f39ace6a1fe7b5d56d0459fb82f8c3
Christian-Alexander-BB <S10205535@connect.np.edu.sg>
2021-02-06 20:16:59 +0800

    Get ID of Clicked Button

--- js/script_front.js
+++ js/script_front.js
@@ -40,6 +40,52 @@ $(document).ready(function () {
         closeNav();
     });
 
+    $("#task-result").on("click", ".play-button", function(e) {
+        console.log(e.target.id);
+    });
+
+    function timeToString(time) {
+        let diffInHrs = time / 3600000;
+        let hh = Math.floor(diffInHrs);
+
+        let diffInMins = (diffInHrs - hh) * 60;
+        let mm = Math.floor(diffInMins);
+
+        let diffInSecs = (diffInMins - mm) * 60;
+        let ss = Math.floor(diffInSecs);
+
+        let diffInMs = (diffInSec - ss) * 1000;
+        let ms = Math.floor(diffInMs);
+
+        let formattedHH = hh.toString().padstart(2, "0");
+        let formattedMM = mm.toString().padStart(2, "0");
+        let formattedSS = ss.toString().padStart(2, "0");
+
+        return `${formattedHH}:${formattedMM}:${formattedSS}`;
+    }
+
+    let startTime;
+    let elapsedTime;
+    let timerInterval;
+
+    function print(txt) {
+        document.getElementById(displayVal).innerHTML = txt;
+    }
+
+    function start() {
+        let displayVal = "gay"
+
+        startTime = Date.now();
+        timerInterval = setInterval(function printTime() {
+            elapsedTime = Date.now() - startTime;
+            document.getElementById(displayVal).innerHTML = timeToString(elapsedTime);
+        }
+    ), 1000}
+
+    function pause() {
+        clearInterval(timerInterval);
+    }
+
     function openNav() {
         document.getElementById("id-side-nav").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
@@ -51,15 +97,3 @@ $(document).ready(function () {
         document.getElementById("main").style.marginLeft = "0";  
       } 
     });
-
-// function openNav() {
-//     $("#id-side-nav").css(
-//         "width", "250px"
-//     );
-// }
-
-// function closeNav() {
-//     $("#id-side-nav").css(
-//         "width", "0"
-//     );
-// }
